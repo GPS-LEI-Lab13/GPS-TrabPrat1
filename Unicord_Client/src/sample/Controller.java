5045df1cb1c677f1dd063d9816e9f0cff2b2120a
Zigko <cenas.dorin2@hotmail.com>
2020-12-19 16:16:19 +0000

    add controllers to fxmls

--- Unicord_Client/src/sample/Controller.java
+++ Unicord_Client/src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.scene.control.Alert;
+
+import java.net.Socket;
+
 public class Controller {
+    private Socket socket;
+    private final MainReceiver mainReceiver;
+    private User  user;
+    private List<Channel> channels;
+
+    void openMessageDialog(Alert.AlertType type, String title, String message){
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(message);
+
+        alert.showAndWait();
+    }
 }
--- Unicord_Client/src/sample/MainWindow.java
+++ Unicord_Client/src/sample/MainWindow.java
@@ -1,4 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class MainWindow {
+    public void createMenuItem(ActionEvent actionEvent) {
+    }
+
+    public void aboutMenuItem(ActionEvent actionEvent) {
+    }
 }
