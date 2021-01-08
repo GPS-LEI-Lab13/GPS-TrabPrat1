b9d211010fd1068f6a642ed6a7944af45c93a5bf
Pedro dos Santos Alves <51165769+ShadowXPA@users.noreply.github.com>
2021-01-08 11:46:25 +0000

    Update Validator.java

--- Unicord_Client/src/pt/isec/Validator.java
+++ Unicord_Client/src/pt/isec/Validator.java
@@ -1,11 +1,16 @@
+/*
+ * Validator
+ * 
+ * Version 1.2
+ * 
+ * Unicord
+ */
 package pt.isec;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-
-    private Validator(){}
 
     public static boolean checkUserPasswordRules(String password){
         Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@.$#!?/|&\\\\%()]).{8,25}$");
@@ -16,4 +21,6 @@ public class Validator {
     public static boolean checkUsernameRules(String username){
         return username.length() >= 6 && username.length() <= 25;
     }
+
+    private Validator(){}
 }
